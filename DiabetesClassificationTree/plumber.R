e7f54d765219a5a5874e10ee223d320dd2ed015f
Zmginder <zachary.ginder@gmail.com>
2025-07-29 17:51:30 -0400

    Update

--- DiabetesClassificationTree/plumber.R
+++ DiabetesClassificationTree/plumber.R
@@ -93,14 +93,27 @@ finaltuned_classificationtree <- prune(finalfull_classificationtree, cp=cp)
 #* @apiDescription This API utilizes a classification tree model to determine whether a respondent is likely to have prediabetes/diabetes or not. Because it is a classification tree, all the parameters must be specified even though not all parameters were chosen as decision rules in the classification tree model.
 
 #* Make a prediction
-#* @param HighBP Class for whether the respondent has high blood pressure
-#* @param GenHlth Class for respondents general health
-#* @param BMI The respondents numeric BMI
-#* @param HighChol Class for whether the respondent has high cholersterol
-#* @param Age Class of the respondents age
-#* @param CholCheck Class for whether the respondent has had a cholesterol check in the last 5 years
-#* @param HvyAlcoholConsump Class for whether the respondent is a heavy alcohol consumer
-#* @param Income Class for what income bracket the respondent falls into
+#* @param HighBP Class for whether the respondent has high blood pressure (No_High_BP,High_BP)
+#* @param HighChol Class for whether the respondent has high cholesterol (No_High_Cholesterol,High_Cholesterol)
+#* @param CholCheck Class for whether the respondent has had a cholesterol check in the last 5 years (No_Cholesterol_Check_In_5_Years,Yes_Cholesterol_Check_In_5_Years)
+#* @param BMI Respondents numeric BMI
+#* @param Smoker Class for whether the respondent has smoked at least 100 cigarettes in their life (Smoker, Not_A_Smoker)
+#* @param Stroke Class for whether the respondent has ever had a stroke (Diagnosed_With_Stroke,No_Stroke)
+#* @param HeartDiseaseorAttack Class for whether the respondent has coronary heart disease or had myocardial infarction (Coronary_Heart_Disease_or_Myocardial_Infarction,No_Heart_Disease_Evident)
+#* @param PhysActivity Class for whether the respondent has had physical activity in the last 30 days not including job activity (Physical_Activity_in_Past_30_Days,No_Physical_Activity_in_Past_30_Days)
+#* @param Fruits Class whether the respondent consumes fruit 1 or more times per day (Consume_Fruit_1_Or_More_Times_Per_Day,Does_Not_Consume_Fruits_Regularly)
+#* @param Veggies Class whether the respondent consumes vegetables 1 or more times per day (Consume_Vegetables_1_Or_More_Times_Per_Day,Does_Not_Consume_Vegetables_Regularly)
+#* @param HvyAlcoholConsump Class whether the respondent is a heavy alcohol consumer defined by >=14 drinks per week for adult men and >=7 adult drinks per week for adult women (Heavy_Alcohol_Consumer,Not_Heavy_Alcohol_Consumer)
+#* @param AnyHealthcare Class on whether the respondent has any kind of health coverage including health insurance, prepaid plans such as HMO, etc.(Has_Health_Insurance,No_Health_Insurance)
+#* @param NoDocbcCost Class for whether the respondent could not see a doctor in the last 12 months because of cost (Missed_Doctors_Visit_Due_to_Cost,No_Cost_Barrier_to_Healthcare)
+#* @param GenHlth Class for the respondents general health (Excellent,Very_Good,Good,Fair,Poor)
+#* @param MentHlth Numeric days the respondent experienced poor mental health (1-30)
+#* @param PhysHlth Numeric days the respondent experienced physical illness or injury (1-30)
+#* @param DiffWalk Class for whether the respondent has difficult walking or climbing stairs (Difficulty_With_Walking_or_Stairs,No_Difficulty_With_Walking_or_Stairs)
+#* @param Sex Class for the respondents sex (Female,Male)
+#* @param Age Class for the respondents age (18-24,25-29,30-34,35-39,40-44,45-49,50-54,55-59,60-64,65-69,70-74,75-79,80_or_older)
+#* @param Education Class for the respondents highest completed education level (Never_Attended_School_Or_Only_Kindergarten,Grades_1-8,Grades_ 9-11,Grades_12_or_GED,Some_College_or_Technical_School,Graduated_College_or_Technical_School)
+#* @param Income Class for the respondents income bracket (Less_than_$10,000,$10,000_to_less_than_$15,000,$15,000_to_less_than_$20,000,$20,000_to_less_than_$25,000,$25,000_to_less_than_$35,000,$35,000_to_less_than_$50,000,$50,000_to_less_than_$75,000,$75,000_or_more)
 #* @get /pred
 function(HighBP="No_High_BP",
          HighChol="No_High_Cholesterol",
@@ -148,21 +161,11 @@ function(HighBP="No_High_BP",
     prediction<-predict(finaltuned_classificationtree,newdata=pred_data,type="class")
     return(prediction)
 }
-
-#* Plot a histogram
-#* @serializer png
-#* @get /plot
-function() {
-    rand <- rnorm(100)
-    hist(rand)
-}
-
-#* Return the sum of two numbers
-#* @param a The first number to add
-#* @param b The second number to add
-#* @post /sum
-function(a, b) {
-    as.numeric(a) + as.numeric(b)
+#* Provide Github Pages URL
+#* @get /info
+function(){
+  "Zach Ginder"
+  "https://zmginder.github.io/Project-3/EDA.html"
 }
 
 # Programmatically alter your API
